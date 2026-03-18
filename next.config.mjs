f493222adc026d212c562891465d9753be3d3388
sneha baberwal <Snehasoni>
2026-03-18 16:02:47 +0530

    Final captcha commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,6 @@
 const nextConfig = {
   experimental: {
     turbopackFileSystemCacheForDev: true,
-    allowedDevOrigins: ["http://192.168.1.95:3000", "http://localhost:3000"],
-
-    serverActions: {
-      allowedOrigins: ["http://192.168.1.95:3000", "http://localhost:3000"],
-    },
   },
 };
 
